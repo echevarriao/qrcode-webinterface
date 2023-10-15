be6474da9907503c82a0253a45916e06adca837d
O. Echevarria <spanishgentleman@yahoo.com>
2023-10-15 10:10:32 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+
+</head>
+<body>
+<form action = "test.php" method = "get">
+<p>
+	<label>Enter URL <input type = "text" name = "url" id = "url" /></label>
+</p>
+<p>
+	Use UConn Logo: 
+	<label><input type = "radio" name = "uselogo" id = "uselogo" value = "1" checked /> Yes</label>
+	<label><input type = "radio" value = "0" name = "uselogo" id = "uselogo" /> No</label>
+</p>
+<input type = "submit" name = "submitButton" id = "submitButton" value = "submit" />
+</form>
+</body>
+</html>
